6af8c08e9b2367d167ac72a8c1e81b5800baecae
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-11-10 16:28:39 +0000

    Update Shows Router

--- routes/shows.js
+++ routes/shows.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const showsRouter = Router();
-const { Show, User } = require("../models/index")
+const { Show, User, watched } = require("../models/index")
 const { db } = require("../db/connection")
 
 showsRouter.get("/", async (req, res) => {
@@ -16,10 +16,77 @@ showsRouter.get("/:id", async (req, res) => {
     res.json(findShow);
 });
 
-showsRouter.put("/:id", async (req, res) => {
-    const updatedShow = await Show.update(req.body, {where: {id: req.params.id}});
-    const shows = await Show.findAll();
-    res.json(shows);
+showsRouter.get('/genres/:genre', async (req, res) => {
+    try {
+        const userEnteredGenre = req.params.genre.toLowerCase();
+        let genre;
+        if (userEnteredGenre.includes("sci")) {
+            genre = "Science Fiction";
+        } else {
+            genre =
+                userEnteredGenre.charAt(0).toUpperCase() +
+                userEnteredGenre.slice(1).toLowerCase();
+        }
+        const shows = await Show.findAll({
+            where: {
+                genre: genre
+            }
+        });
+        if (shows.length === 0) {
+            return res.status(404).json({ message: `No shows found with genre: ${genre}` });
+        }
+        res.status(200).json(shows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+showsRouter.put('/:id/watched', async (req, res) => {
+    const showId = req.params.id;
+    try {
+        const showToUpdate = await Show.findByPk(showId);
+        if (!showToUpdate) {
+            return res.status(404).json({ error: 'Show not found' });
+        }
+        showToUpdate.rating = req.body.rating || showToUpdate.rating;
+        await showToUpdate.save();
+        return res.status(200).json(showToUpdate);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+showsRouter.put('/:id/updates', async (req, res) => {
+  const showId = req.params.id;
+  try {
+    const show = await Show.findByPk(showId);
+    if (!show) {
+      return res.status(404).json({ error: 'Show not found' });
+    }
+    show.available = !show.available;
+    await show.save();
+    res.json({ message: 'Show status updated successfully', show });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+showsRouter.delete('/:id', async (req, res) => {
+    const showId = req.params.id;
+    try {
+        const showToDelete = await Show.findByPk(showId);
+        if (!showToDelete) {
+            return res.status(404).json({ error: 'Show not found' });
+        }
+        await showToDelete.destroy();
+        res.status(200).json({ message: 'Show deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 module.exports = { showsRouter }
